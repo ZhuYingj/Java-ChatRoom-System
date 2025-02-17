b08bea53d1fc4529bba1dc3616ae4e92ab431b1e
Sanchez <bryanschez@gmail.com>
2025-02-17 16:55:39 -0500

    fix: added a random ip adress to be able to run the server and client

--- environment/serverPort.java
+++ environment/serverPort.java
@@ -1,7 +1,6 @@
 package environment;
 
-
 public class serverPort {
-    public static String serverAddress = "Mettez votre adresse ip";
+    public static String serverAddress = "127.69.69.69";
     public static int serverPort = 5005;
 }
