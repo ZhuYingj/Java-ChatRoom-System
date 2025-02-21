8164277cc180aacb9893f695a3efce956ca12298
Sanchez <bryanschez@gmail.com>
2025-02-21 01:55:51 -0500

    fix: changed the type of the userData file from txt to csv

--- server/src/DataPathConst.java
+++ server/src/DataPathConst.java
@@ -1,6 +1,6 @@
 package server.src;
 
 public class DataPathConst {
-    public static String USERS_DATA_PATH = "server\\environment\\userData.txt";
+    public static String USERS_DATA_PATH = "server\\environment\\userData.csv";
     public static String MESSAGES_DATA_PATH = "server\\environment\\messages.csv";
 }
